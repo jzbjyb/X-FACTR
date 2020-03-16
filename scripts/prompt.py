4a21d24c25dd29eada7b41f45f4a4419ef5e1d36
jzb <zhengbaj@cs.cmu.edu>
2020-03-16 19:09:10 -0400

    add fr inflect model

--- scripts/prompt.py
+++ scripts/prompt.py
@@ -315,6 +315,17 @@ class PromptRU(Prompt):
     SUFS = {"й", "б", "в", "г", "д", "ж", "з", "к", "л", "м", "н", "п", "р", "с", "т", "ф", "х", "ц", "ч", "ш", "щ"}
 
 
+    def __init__(self,
+                 entity2lang: Dict[str, Gender],
+                 entity2instance: Dict[str, str],
+                 disable_inflection: str=False,
+                 disable_article: bool=False):
+        super().__init__(entity2lang=entity2lang,
+                         entity2instance=entity2instance,
+                         disable_inflection=disable_inflection,
+                         disable_article=disable_article)
+
+
     # Decide on Russian gender for the unknown entities based on the endings
     # Based on http://www.study-languages-online.com/russian-nouns-gender.html
     @staticmethod
@@ -341,17 +352,6 @@ class PromptRU(Prompt):
                 gender = "MASC"
 
         return gender, number
-
-
-    def __init__(self,
-                 entity2lang: Dict[str, Gender],
-                 entity2instance: Dict[str, str],
-                 disable_inflection: str=False,
-                 disable_article: bool=False):
-        super().__init__(entity2lang=entity2lang,
-                         entity2instance=entity2instance,
-                         disable_inflection=disable_inflection,
-                         disable_article=disable_article)
 
 
     @overrides
@@ -484,5 +484,301 @@ class PromptRU(Prompt):
                     elif "Lgspec1" in w:
                         form2 = cache_inflect(lemma, f"ADJ;{gender};SG;LGSPEC1", language='rus')[0]
                 words[i] = form2
+
+        return ' '.join(words), label
+
+
+class PromptFR(Prompt):
+    def __init__(self,
+                 entity2lang: Dict[str, Gender],
+                 entity2instance: Dict[str, str],
+                 disable_inflection: str=False,
+                 disable_article: bool=False):
+        super().__init__(entity2lang=entity2lang,
+                         entity2instance=entity2instance,
+                         disable_inflection=disable_inflection,
+                         disable_article=disable_article)
+        self.article: Dict[str, str] = {}
+        with open('data/lang_resource/fr/articles.txt') as inp:
+            for l in inp:
+                l = l.strip().split('\t')
+                self.article[l[1]] = l[0]
+
+    @staticmethod
+    def starts_with_vowel(w):
+        w = w.strip()
+        if w[0] in {'a', 'o', 'i', 'e', 'u', 'y', 'à', 'è', 'ù', 'é', 'â', 'ê', 'î', 'ô', 'û'}:
+            return True
+        return False
+
+
+    @staticmethod
+    def gender_heuristic(w):
+        w = w.strip()
+        if ' ' not in w:
+            if w[-6:] in {"ouille"}:
+                return "FEM"
+            elif w[-5:] in {"aisse", "ousse", "aille", "eille", "ouche", "anche"}:
+                return "FEM"
+            elif w[-4:] in {"asse", "esse", "isse", "ance", "anse", "ence", "once", "enne", "onne", "aine", "eine",
+                            "erne", "ande", "ende", "onde", "arde", "orde", "euse", "ouse", "aise", "oise", "ache",
+                            "iche", "ehce", "oche", "uche", "iere", "eure", "atte", "otte", "oute", "orte", "ante",
+                            "ente", "inte", "onte", "alle", "elle", "ille", "olle", "appe", "ampe", "ombe", "igue"}:
+                return "FEM"
+            elif w[-4:] in {"aume", "isme", "ours", "euil", "ueil"}:
+                return "MASC"
+            elif w[-3:] in {"aie", "oue", "eue", "ace", "ece", "ice", "une", "ine", "ade", "ase", "ese", "ise", "yse",
+                            "ose", "use", "ave", "eve", "ive", "ete", "ête"}:
+                return "FEM"
+            elif w[-3:] in {"and", "ant", "ent", "int", "ond", "ont", "eau", "aud", "aut", "ais", "ait", "out", "oux",
+                            "age", "ege", "ème", "ome", "est", "eul", "all", "air", "erf", "ert", "arc", "ars", "art",
+                            "our", "ord", "ors", "ort", "oir", "eur", "ail", "eil", "ing"}:
+                return "MASC"
+            elif w[-2:] in {"te", "ée", "ie", "ue"}:
+                return "FEM"
+            elif w[-2:] in {"an", "in", "om", "on", "au", "os", "ot", "ai", "es", "et", "ou", "il", "it", "is", "at",
+                            "as", "us", "ex", "al", "el", "ol", "if", "ef", "ac", "ic", "oc", "uc", "um", "am", "en"}:
+                return "MASC"
+            elif w[-1] in {"o", "i", "y", "u"}:
+                return "MASC"
+            else:
+                return "MASC"
+            # ARGH
+        else:
+            w2 = w.split(' ')[0]
+            if w2[-6:] in {"ouille"}:
+                return "FEM"
+            elif w2[-5:] in {"aisse", "ousse", "aille", "eille", "ouche", "anche"}:
+                return "FEM"
+            elif w2[-4:] in {"asse", "esse", "isse", "ance", "anse", "ence", "once", "enne", "onne", "aine", "eine",
+                             "erne", "ande", "ende", "onde", "arde", "orde", "euse", "ouse", "aise", "oise", "ache",
+                             "iche", "ehce", "oche", "uche", "iere", "eure", "atte", "otte", "oute", "orte", "ante",
+                             "ente", "inte", "onte", "alle", "elle", "ille", "olle", "appe", "ampe", "ombe", "igue"}:
+                return "FEM"
+            elif w2[-4:] in {"aume", "isme", "ours", "euil", "ueil"}:
+                return "MASC"
+            elif w2[-3:] in {"aie", "oue", "eue", "ace", "ece", "ice", "une", "ine", "ade", "ase", "ese", "ise", "yse",
+                             "ose", "use", "ave", "eve", "ive", "ete", "ête"}:
+                return "FEM"
+            elif w2[-3:] in {"and", "ant", "ent", "int", "ond", "ont", "eau", "aud", "aut", "ais", "ait", "out", "oux",
+                             "age", "ege", "ème", "ome", "est", "eul", "all", "air", "erf", "ert", "arc", "ars", "art",
+                             "our", "ord", "ors", "ort", "oir", "eur", "ail", "eil", "ing"}:
+                return "MASC"
+            elif w2[-2:] in {"te", "ée", "ie", "ue"}:
+                return "FEM"
+            elif w2[-2:] in {"an", "in", "om", "on", "au", "os", "ot", "ai", "es", "et", "ou", "il", "it", "is", "at",
+                             "as", "us", "ex", "al", "el", "ol", "if", "ef", "ac", "ic", "oc", "uc", "um", "am", "en"}:
+                return "MASC"
+            elif w2[-1] in {"o", "i", "y", "u"}:
+                return "MASC"
+            else:
+                return "MASC"
+            # ARGH
+
+
+    def get_ender_number(self, uri: str, label: str) -> Tuple[str, str, bool, bool, bool]:
+        number = "SG"
+        country = False
+        city = False
+        proper = True
+        gender = self.GENDER_MAP[self.entity2lang[uri]]
+
+        if gender == 'NEUT':  # use heuristics
+            gender = self.gender_heuristic(label)
+            if uri in self.entity2instance:
+                if 'state' in self.entity2instance[uri] or 'country' in self.entity2instance[uri]:
+                    country = True
+                    proper = True
+                elif 'business' in self.entity2instance[uri]:
+                    gender = "FEM"
+                    proper = True
+                elif 'enterprise' in self.entity2instance[uri]:
+                    gender = "FEM"
+                    proper = True
+                elif 'city' in self.entity2instance[uri]:
+                    city = True
+                    proper = True
+                elif 'human' in self.entity2instance[uri]:
+                    gender = "MASC"
+                elif 'island' in self.entity2instance[uri]:
+                    proper = True
+                elif 'musical group' in self.entity2instance[uri]:
+                    proper = True
+                elif 'record label' in self.entity2instance[uri]:
+                    proper = True
+                elif 'language' in self.entity2instance[uri]:
+                    gender = "MASC"
+                elif 'sports team' in self.entity2instance[uri]:
+                    gender = "FEM"
+                    proper = True
+                elif 'automobile manufacturer' in self.entity2instance[uri]:
+                    gender = "FEM"
+                    proper = True
+                elif 'football club' in self.entity2instance[uri]:
+                    gender = "FEM"
+                    proper = True
+
+        return gender, number, country, city, proper
+
+
+    @overrides
+    def fill_x(self, prompt: str, uri: str, label: str) -> Tuple[str, str]:
+        ent_gender, ent_number, ent_country, ent_city, ent_proper = self.get_ender_number(uri, label)
+
+        if label.isupper():
+            do_not_inflect = True
+        else:
+            do_not_inflect = False
+
+        words = prompt.split(' ')
+
+        if '[X]' in words:
+            i = words.index('[X]')
+            words[i] = label
+
+        # Now also check the correponding articles, if they exist
+        has_article: bool = False
+        if "[ARTDEF;X]" in words:
+            has_article = True
+            i = words.index('[ARTDEF;X]')
+            vowel = self.starts_with_vowel(words[i + 1])
+            if ent_proper and not ent_country:
+                # Paul: If X is a proper noun (but not a country), then drop the article altogether
+                del words[i]
+            elif vowel:
+                has_article = True
+                art = self.article["ARTDEF;VOWEL"]
+            elif ent_number == "SG":
+                has_article = True
+                art = self.article[f"ARTDEF;{ent_gender};{ent_number}"]
+            else:
+                has_article = True
+                art = self.article[f"ARTDEF;{ent_number}"]
+        if "[PREPDEF;X]" in words:
+            i = words.index('[PREPDEF;X]')
+            if ent_proper and not ent_country:
+                has_article = True
+                art = self.article[f"PREPDEF;PROPN"]
+            elif ent_number == "SG":
+                has_article = True
+                art = self.article[f"PREPDEF;{ent_gender};{ent_number}"]
+            else:
+                has_article = True
+                art = self.article[f"PREPDEF;{ent_number}"]
+        if "[PREPDEF-à;X]" in words:
+            i = words.index('[PREPDEF-à;X]')
+            if ent_proper and not ent_country:
+                # Paul: If Y is a proper noun (but not a country), then just à
+                has_article = True
+                art = self.article[f"PREPDEF-à;PROPN"]
+            elif ent_number == "SG":
+                has_article = True
+                art = self.article[f"PREPDEF-à;{ent_gender};{ent_number}"]
+            else:
+                has_article = True
+                art = self.article[f"PREPDEF-à;{ent_number}"]
+
+        if has_article:
+            if self.disable_article:
+                words[i] = ''
+            else:
+                words[i] = art
+
+        # Now also check the corresponfing verbs, if they exist.
+        # Needed for subject-verb agreement
+        for i, w in enumerate(words):
+            if w[0] == '[' and 'X-Gender' in w:
+                if '|' in w:
+                    options = w.strip()[1:-1].split('|')
+                    if ent_gender == "MASC":
+                        form = options[0].strip().split(';')[0]
+                        words[i] = form
+                    else:
+                        form = options[1].strip().split(';')[0]
+                        words[i] = form
+
+        return ' '.join(words), label
+
+
+    def fil_y(self, prompt: str, uri: str, label: str,
+              num_mask: int=0, mask_sym: str='[MASK]') -> Tuple[str, str]:
+        ent_gender, ent_number, ent_country, ent_city, ent_proper = self.get_ender_number(uri, label)
+
+        if label.isupper():
+            do_not_inflect = True
+        else:
+            do_not_inflect = False
+
+        words = prompt.split(' ')
+
+        if '[Y]' in words:
+            i = words.index('[Y]')
+            words[i] = label
+
+        # Now also check the correponsing articles, if they exist
+        has_article: bool = False
+        if "[ARTDEF;Y]" in words:
+            i = words.index('[ARTDEF;Y]')
+            vowel = self.starts_with_vowel(words[i + 1])
+            if ent_proper and not ent_country:
+                # Paul: If X is a proper noun (but not a country), then drop the article altogether
+                del words[i]
+            elif vowel:
+                has_article = True
+                art = self.article["ARTDEF;VOWEL"]
+            elif ent_number == "SG":
+                has_article = True
+                art = self.article[f"ARTDEF;{ent_gender};{ent_number}"]
+            else:
+                has_article = True
+                art = self.article[f"ARTDEF;{ent_number}"]
+        if "[ARTIND;Y]" in words:
+            i = words.index('[ARTIND;Y]')
+            if ent_number == "SG":
+                has_article = True
+                art = self.article[f"ARTIND;{ent_gender};{ent_number}"]
+            else:
+                has_article = True
+                art = self.article[f"ARTIND;{ent_number}"]
+        if "[PREPDEF;Y]" in words:
+            i = words.index('[PREPDEF;Y]')
+            if ent_proper and not ent_country:
+                has_article = True
+                art = self.article[f"PREPDEF;PROPN"]
+            elif ent_number == "SG":
+                has_article = True
+                art = self.article[f"PREPDEF;{ent_gender};{ent_number}"]
+            else:
+                has_article = True
+                art = self.article[f"PREPDEF;{ent_number}"]
+        if "[PREPDEF-à;Y]" in words:
+            i = words.index('[PREPDEF-à;Y]')
+            if ent_proper and not ent_country:
+                # Paul: If Y is a proper noun (but not a country), then just à
+                has_article = True
+                art = self.article[f"PREPDEF-à;PROPN"]
+            elif ent_number == "SG":
+                has_article = True
+                art = self.article[f"PREPDEF-à;{ent_gender};{ent_number}"]
+            else:
+                has_article = True
+                art = self.article[f"PREPDEF-à;{ent_number}"]
+        if "[PREPLOC;Y]" in words:
+            i = words.index('[PREPLOC;Y]')
+            if ent_city:
+                has_article = True
+                art = self.article[f"PREPLOC;CITY"]
+            elif ent_country:
+                has_article = True
+                art = self.article[f"PREPLOC;COUNTRY"]
+            else:
+                has_article = True
+                art = self.article[f"PREPLOC;COUNTRY"]
+
+        if has_article:
+            if self.disable_article:
+                words[i] = ''
+            else:
+                words[i] = art
 
         return ' '.join(words), label
