07f01cfbe5ccd8618bd042d59ad9dbf49e90d7b8
jzb <zhengbaj@cs.cmu.edu>
2020-04-15 21:04:26 -0400

    update trex dataset

--- build_data.sh
+++ build_data.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
+probe=mTREx
+
 # multi word lama
-# Q11720 has a bug
+# Q11720, Q5136332 have bugs
 python scripts/entity_lang.py --task get_lang \
-    --inp data/mTREx/sub:data/mTREx_multi_rel.txt \
-    --out data/mTREx_unicode_escape.txt
+    --inp data/${probe}/sub:data/${probe}_multi_rel.txt \
+    --out data/${probe}_unicode_escape.txt
 
 python scripts/check_gender.py \
-    --inp data/mTREx_unicode_escape.txt \
-    --out data/mTREx_gender.txt
+    --inp data/${probe}_unicode_escape.txt \
+    --out data/${probe}_gender.txt
 
 python scripts/check_instanceof.py \
-    --inp data/mTREx_unicode_escape.txt \
-    --out data/mTREx_instanceof.txt
+    --inp data/${probe}_unicode_escape.txt \
+    --out data/${probe}_instanceof.txt
 
 for lang in en el es fr ja ko mr nl zh; do
     python scripts/entity_lang.py --task get_alias \
-        --inp data/mTREx_unicode_escape.txt \
-        --out data/alias/mTREx/${lang}.txt \
+        --inp data/${probe}_unicode_escape.txt \
+        --out data/alias/${probe}/${lang}.txt \
         --lang ${lang}
 done
